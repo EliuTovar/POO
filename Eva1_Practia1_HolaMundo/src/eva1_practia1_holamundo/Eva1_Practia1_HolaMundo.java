91a55943eb532c855f5c731450099dd45095aacb
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-02-25 11:36:44 -0600

    Eva1_Practica1_HolaMundo
    Actividad en clase

---
+++ Eva1_Practia1_HolaMundo/src/eva1_practia1_holamundo/Eva1_Practia1_HolaMundo.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practia1_holamundo;
+
+/**
+ *
+ * @author tovar
+ */
+/*
+    PALABRAS EN AZUL: palabras reservadas: usoo exclusivo del lenguaje
+    PALABRAS EN NEGRO: IDENTIFICADORES --> variables, constantes, clases, funciones, objetos, etc
+    []: arreglo
+    []: delimitadores -> bloques de codigo
+
+*/
+public class Eva1_Practia1_HolaMundo { //CUERPO DE UNA CLASE
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {//FUNCION
+        // TODO code application logic here
+        System.out.println("Hello world!!");
+    }
+    
+}
