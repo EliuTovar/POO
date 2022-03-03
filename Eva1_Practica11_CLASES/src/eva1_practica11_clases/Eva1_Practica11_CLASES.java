c0ca0a4f83c7d47acc8f0a1c3dc97bc66759a62c
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-03-03 10:56:52 -0600

    Eva1_Practica11_CLASES
    Practica en clase

---
+++ Eva1_Practica11_CLASES/src/eva1_practica11_clases/Eva1_Practica11_CLASES.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva1_practica11_clases;
+
+/**
+ *
+ * @author tovar
+ */
+public class Eva1_Practica11_CLASES {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+class Persona{
+    private String id;
+    private String nombre;
+    private int edad;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+    
+    
+}
