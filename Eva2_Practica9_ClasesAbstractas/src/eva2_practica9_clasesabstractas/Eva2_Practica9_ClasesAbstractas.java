b682f7c300a325eb3320c2622e8a85de12264592
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-05-08 00:12:00 -0500

    Add files via upload

---
+++ Eva2_Practica9_ClasesAbstractas/src/eva2_practica9_clasesabstractas/Eva2_Practica9_ClasesAbstractas.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica9_clasesabstractas;
+
+/**
+ *
+ * @author tovar
+ */
+public class Eva2_Practica9_ClasesAbstractas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Cliente cliente = new Cliente("23132323", "Eliu", 10);
+        cliente.mostrarDatos();
+        
+        
+    }
+    
+}
+
+
