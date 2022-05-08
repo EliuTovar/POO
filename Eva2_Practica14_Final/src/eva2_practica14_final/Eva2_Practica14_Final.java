ac31be96c682f9658d79558d5ab8132a5dae9561
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-05-08 00:14:53 -0500

    Add files via upload

---
+++ Eva2_Practica14_Final/src/eva2_practica14_final/Eva2_Practica14_Final.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package eva2_practica14_final;
+
+/**
+ *
+ * @author tovar
+ */
+public class Eva2_Practica14_Final {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
+
+class Persona{
+    
+    
+    
+    
+}
