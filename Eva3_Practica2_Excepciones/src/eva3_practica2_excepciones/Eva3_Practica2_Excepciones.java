ecf4f60d8323d29fd1f47e74efe1e219a0723828
EliuTovar <99421840+EliuTovar@users.noreply.github.com>
2022-05-08 00:19:32 -0500

    Add files via upload

---
+++ Eva3_Practica2_Excepciones/src/eva3_practica2_excepciones/Eva3_Practica2_Excepciones.java
@@ -0,0 +1,18 @@
+
+package eva3_practica2_excepciones;
+
+import java.util.Scanner;
+
+
+public class Eva3_Practica2_Excepciones {
+
+    public static void main(String[] args) {
+        
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.print("Ingrese su edad(Numero entero):");
+        int edad = entrada.nextInt();
+        System.out.println("Tu edad es: " + edad);
+    }
+    
+}
